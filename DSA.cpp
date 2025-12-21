1e599a9dd7d0d6cb2f3c2068ac299f555cbc0cda
Aditi Jha <aditijha934@gmail.com>
2025-12-22 00:28:19 +0530

    Pattern problems for day 1

--- DSA.cpp
+++ DSA.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n = 3;
+    int n = 4;
+
     for (int i = 0; i < n; i++) {
-        char ch = 'A';
-        for (int j = 0; j < n; j++) {
-            cout << ch;
-            ch = ch + 1;
+        for (int j = i+1; j > 0 ; j--) {
+            cout << j << " ";
         }
         cout << endl;
     }
