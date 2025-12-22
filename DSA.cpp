98ada8d313c2320e597ff9eeb2a742fec71f5251
Aditi Jha <aditijha934@gmail.com>
2025-12-22 22:35:15 +0530

    Patterns- Part II

--- DSA.cpp
+++ DSA.cpp
@@ -4,8 +4,8 @@ int main() {
     int n = 4;
 
     for (int i = 0; i < n; i++) {
-        for (int j = i+1; j > 0 ; j--) {
-            cout << j << " ";
+        for ( int j = i+1; j > 0; j--){
+            cout << j;
         }
         cout << endl;
     }
