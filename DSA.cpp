41ffae40d2130bfd06ed8b9c13b487b15236f983
Aditi Jha <aditijha934@gmail.com>
2025-12-23 11:21:18 +0530

    191 questions of CPP

--- DSA.cpp
+++ DSA.cpp
@@ -1,14 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main() {
-    int n = 4;
 
-    for (int i = 0; i < n; i++) {
-        for ( int j = i+1; j > 0; j--){
-            cout << j;
-        }
-        cout << endl;
-    }
+    cout << "Hello Word";
+
+
 
     return 0;
 }
