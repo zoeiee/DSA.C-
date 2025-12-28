26b30d879d2e77c29b7603dc4f5de020dddd9ba7
Aditi Jha <aditijha934@gmail.com>
2025-12-28 17:16:37 +0530

    Problems

--- DSA.cpp
+++ DSA.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 using namespace std;
-int main() {
 
-    int n = 8;
-    int i;
-    for (i = 1; i <= 10; i++) {
-        cout << n << "*" << i << "=" <<n*i << endl;
+    bool isPrime(int n){
+        if (n<=1)
+            return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+return true;
     }
-    return 0;
-}
+void findPrimes(int l, int r) {
+        bool found = false;
+        for (int i = l; i <= r; i++) {
+            if (isPrime(i)) {
+                cout << i<< endl;
+                found = true;
+
+            }
+        }
+        if (!found) {
+            printf(
+                "No prime numbers found in the given range.");
+        }
+    }
+int main() {
+        int l = 10, r = 30;
+
+        findPrimes(l, r);
+
+        return 0;
+    }
+
+
