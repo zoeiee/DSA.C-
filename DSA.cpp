c79178aacfb2bca61d4dd0821a3ba4f28ca50c52
Aditi Jha <aditijha934@gmail.com>
2026-01-01 22:52:38 +0530

    Phase 1 Logic Building Level 2

--- DSA.cpp
+++ DSA.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    char c;
-    cout << "Enter a character: ";
-    cin >> c;
-    if (c >= 'a' && c <= 'z') {
-        cout << "Lowercase";
-    }
-    else if (c >= 'A' && c <= 'Z') {
-        cout << "Uppercase";
-    }
-    else if ( c >= '0' && c <= '9') {
-        cout << "Digits";
-    }
-    else {
-        cout << "Special character";
-    }
+    int day = 4;
+    switch (day) {
+        case 1: cout << "Monday"; break;
+        case 2: cout << "Tuesday"; break;
 
-}
+
+
+
 
