fe55415654094b877b77fc8a0a6ac445c90a8f84
Aditi Jha <aditijha934@gmail.com>
2026-01-03 22:36:57 +0530

    Logic Building Phase 1 Level 5

--- DSA.cpp
+++ DSA.cpp
@@ -2,25 +2,9 @@
 using namespace std;
 
 int main() {
-    string pwd;
-    cout << "Enter password: ";
-    cin >> pwd;
-    bool valid = pwd.length() >= 8;
-    bool digit = false;
-    for (char c : pwd) {
-        for (char c : pwd) {
-            if (c >= '0' && c <= '9') {
-                digit = true;
-                break;
-            }
-        }
-
-        if (valid && digit)
-            cout << "Password is valid\n";
-        else
-            cout << "Password is invalid\n";
-
-        return 0;
+    int n = 5;
+    for (int i = 1; i <= 10; i++){
+        cout << n * i << endl;
     }
-
+    return 0;
 }
