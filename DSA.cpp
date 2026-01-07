6193ee0318a7abb25c80642861cdc99d3d888e63
Aditi Jha <aditijha934@gmail.com>
2026-01-07 23:23:08 +0530

    Logic Building Gap Day

--- DSA.cpp
+++ DSA.cpp
@@ -2,21 +2,12 @@
 using namespace std;
 
 int main() {
-    int n, temp, sum = 0, r;
-    cin >> n;
-    temp = n;
-
-    while (temp != 0) {
-        r = temp % 10;
-        sum += r * r * r;
-        temp /= 10;
+    int n = 666;
+    int sum = 0;
+    while (n != 0) {
+        int digit = n % 10;
+        sum += digit;
+        n = n / 10;
     }
-
-    if (sum == n)
-        cout << "Armstrong";
-    else
-        cout << "Not Armstrong";
-
-    return 0;
+    cout << sum;
 }
-
