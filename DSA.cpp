570c99f92ce68547cfeab6f7501a91f85e9a9744
Aditi Jha <aditijha934@gmail.com>
2026-01-11 21:26:22 +0530

    revision

--- DSA.cpp
+++ DSA.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int isPrime(int x) {
+    if(x < 2) return 0;
+    for(int i = 2; i * i <= x; i++)
+        if(x % i == 0) return 0;
+    return 1;
+}
+
 int main() {
-    int n, x, count = 0;
+    int n, count = 0;
     cin >> n;
     int a[n];
 
-    for(int i = 0; i < n; i++) cin >> a[i];
-    cin >> x;
-
-    for(int i = 0; i < n; i++)
-        if(a[i] == x) count++;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        if(isPrime(a[i])) count++;
+    }
 
     cout << count;
     return 0;
