29a5e503f6b38bdace4872cf11d48995ee89ab70
Aditi Jha <aditijha934@gmail.com>
2026-01-17 12:03:17 +0530

    Did selection sort using functions and easy algorithm based approaches

--- DSA.cpp
+++ DSA.cpp
@@ -1,89 +1,65 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
-
 int main() {
     int n;
+    cout << "Enter the number of elements in the array: ";
     cin >> n;
+
     int arr[n];
-
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
+        cout << "Enter the element " ;
         cin >> arr[i];
-
-    bool ascending = true;
-    for(int i = 0; i < n - 1; i++) {
-        if(arr[i] > arr[i + 1]) {
-            ascending = false;
-            break;
-        }
     }
+    //selection sort
+    for (int i = 0; i < n-1; i++) {
+        int minIndex = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        //swapping them actually:
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
 
-    if(ascending)
-        cout << "Array is sorted in ascending order";
-    else
-        cout << "Array is not sorted in ascending order";
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+return 0;
+}*/
+/*#include <iostream>
+using namespace std;
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    cout << "After selection sort, the array elements are:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
+
+int main() {
+    int arr[] = {12, 34, 5, 23};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Before selection sort, the array elements are:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    //call
+    selection_sort(arr, n);
     return 0;
-}
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n; cin >> n;
-    int a[n];
-    for(int i=0;i<n;i++) cin >> a[i];
-
-    bool desc = true;
-    for(int i=1;i<n;i++)
-        if(a[i] > a[i-1]) desc = false;
-
-    cout << (desc ? "Descending" : "Not Descending");
-}
-
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    sort(arr, arr + n);
-
-    cout << "Second largest element: " << arr[n - 2];
-
-    return 0;
-}
-
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int main() {
-    int n; cin >> n;
-    int a[n];
-    for(int i=0;i<n;i++) cin >> a[i];
-
-    sort(a, a+n);
-    cout << a[1];
-}
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int main() {
-    int n; cin >> n;
-    int a[n];
-    for(int i=0;i<n;i++) cin >> a[i];
-
-    sort(a, a+n);
-    cout << a[n-1] - a[0];
-}
-
-
-
-
+}*/
 
