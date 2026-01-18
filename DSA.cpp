7685c61fd589f8e7cb6ec63c727c6642c2d53eb1
Aditi Jha <aditijha934@gmail.com>
2026-01-18 21:25:20 +0530

    Merge Sort

--- DSA.cpp
+++ DSA.cpp
@@ -96,4 +96,92 @@ int main() {
     return 0;
 }*/
 
+/*#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
 
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cout << "enter array elements: ";
+        cin>> arr[i];
+    }
+
+for (int i = 1; i < n; i++) {
+    int key = arr[i];
+    int j = i-1;
+
+    while (j >= 0 && arr[j] > key) {
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = key;
+
+}
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+return 0;
+
+}*/
+
+#include <iostream>
+using namespace std;
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i<n1 && j<n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i<n1) {
+        arr[k] = L[i];
+        i++; k++;
+    }
+    while (j<n2) {
+        arr[k] = R[j];
+        j++; k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element ";
+        cin >> arr[i];
+    }
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+}
