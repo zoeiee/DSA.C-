66b79fc1b51bb4f12147ce70c4e5ff3b0c70d709
Aditi Jha <aditijha934@gmail.com>
2026-02-01 12:05:42 +0530

    Second largest

--- DSA.cpp
+++ DSA.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main() {
     int arr[4] = {45,76,12,39};
@@ -9,4 +9,24 @@ int main() {
         }
     }
     cout << largest;
+}*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {34,54,12,78};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int largest = arr[0];
+    int slargest = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            slargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > slargest) {
+            slargest = arr[i];
+        }
+    }
+    cout << slargest << endl;
+    cout << largest << endl;
 }
